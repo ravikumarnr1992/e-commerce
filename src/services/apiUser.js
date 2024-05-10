c5f2f3050cabd62aa3a241858d6c08cdcb0b1bbe
Prajwal S (Trianz) <prajwal.s@trianz.com>
2024-05-10 16:32:05 +0530

    fixed image upload issue and added secrets to env

--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -1,43 +1,38 @@
-import { supabaseUrl } from "./supabase";
- 
-export async function updateUser(userData, id, hasImgUpload) {
- 
-  const hasImagePath = hasImgUpload ? userData?.image?.startsWith?.(supabaseUrl) : false
- 
-  const imageName = hasImgUpload ? `${Math.random()}-${userData?.image?.name}`.replaceAll(
+import supabase, { supabaseUrl } from "./supabase";
+
+export async function updateUser(userData, id, newImgUpload) {
+
+  const imageName = newImgUpload ? `${Math.random()}-${userData?.image?.name}`.replaceAll(
     "/",
     ""
   ) : null
 
-  const imagePath = hasImgUpload ? `${supabaseUrl}/storage/v1/object/public/user-images/${imageName}` : null
+  const imagePath = newImgUpload ? `${supabaseUrl}/storage/v1/object/public/user-images/${imageName}` : null
 
   // 1. Edit user
   let query = supabase.from("login");
- 
-  // A) EDIT
-  if (id && hasImgUpload ) query = query.update({ ...userData, image: imagePath }).eq("id", id);
 
-  if (id && !hasImgUpload ) query = query.update({ ...userData }).eq("id", id);
- 
+  // A) EDIT
+  if (id && newImgUpload) query = query.update({ ...userData, image: imagePath }).eq("id", id);
+
+  if (id && !newImgUpload) query = query.update({ ...userData }).eq("id", id);
+
   const { data, error } = await query.select().single();
- 
+
   if (error) {
     console.error(error);
-    throw new Error("Cabin could not be created");
+    throw new Error("User cannot be updated");
   }
-  // 2. Upload image
-  if (hasImagePath) return data;
- 
-  if(id && hasImgUpload){
+
+  if (id && newImgUpload) {
     const { error: storageError } = await supabase.storage
       .from("user-images")
       .upload(imageName, userData.image);
-   
+
     if (storageError) {
       throw new Error("User image could not be uploaded");
     }
   }
- 
+
   return data;
 }
- 
